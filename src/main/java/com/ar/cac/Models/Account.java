a37baefb37630dd57fbce9fdcc75997b776e7cb4
leo <98562720+lacbj@users.noreply.github.com>
2022-12-07 10:53:58 -0300

    Se agregaron dependencias y Swagger

--- src/main/java/com/ar/cac/Models/Account.java
+++ src/main/java/com/ar/cac/Models/Account.java
@@ -1,15 +1,10 @@
 package com.ar.cac.Models;
 
-import javax.persistence.Column;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
-import com.ar.cac.utils.AccountType;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,16 +16,12 @@ import lombok.Data;
 public class Account {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long id;
 	public String name;
-	@Column(name = "ammount")
-	public Double amount;
+	public Double ammount;
 	public String cbu;
 	
 	public Account () {}
-	
-	
-	
 	
 }
