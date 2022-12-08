dd279079109208ce59269f3f42ce006618a2f520
leo <98562720+lacbj@users.noreply.github.com>
2022-12-08 11:51:32 -0300

    first commit

--- src/main/java/com/ar/cac/Models/Menu.java
+++ src/main/java/com/ar/cac/Models/Menu.java
@@ -16,7 +16,7 @@ import lombok.Data;
 public class Menu {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	
 	public Long id;
 	public String plato;
